62e229dd0dee12e9c4b3427c71efafc6be5c3631
Michael Burton <hello.michaelburton@gmail.com>
2015-07-14 22:35:21 +0100

    Merge works referenceing download and then spits into new for now.

--- main.js
+++ main.js
@@ -1,179 +1,46 @@
 var fs = require('fs');
 var moment = require('moment');
 var formatJson = require('format-json');
+var extend = require('extend');
 
-var langs = [{
-    "preflang": "ar",
-    "locale": "ar"
-}, {
-    "preflang": "bg",
-    "locale": "bg"
-}, {
-    "preflang": "br",
-    "locale": "pt-br"
-}, {
-    "preflang": "ca",
-    "locale": ""
-}, {
-    "preflang": "ca",
-    "locale": "ca"
-}, {
-    "preflang": "cs",
-    "locale": "cs"
-}, {
-    "preflang": "da",
-    "locale": "da"
-}, {
-    "preflang": "de",
-    "locale": "de"
-}, {
-    "preflang": "ee",
-    "locale": "et"
-}, {
-    "preflang": "en",
-    "locale": "en-gb"
-}, {
-    "preflang": "es",
-    "locale": "es"
-}, {
-    "preflang": "fi",
-    "locale": "fi"
-}, {
-    "preflang": "fr",
-    "locale": "fr"
-}, {
-    "preflang": "gr",
-    "locale": "el"
-}, {
-    "preflang": "he",
-    "locale": "he"
-}, {
-    "preflang": "hr",
-    "locale": "hr"
-}, {
-    "preflang": "hu",
-    "locale": "hu"
-}, {
-    "preflang": "id",
-    "locale": "id"
-}, {
-    "preflang": "is",
-    "locale": "is"
-}, {
-    "preflang": "it",
-    "locale": "it"
-}, {
-    "preflang": "jp",
-    "locale": "ja"
-}, {
-    "preflang": "ko",
-    "locale": "ko"
-}, {
-    "preflang": "lt",
-    "locale": "lt"
-}, {
-    "preflang": "lv",
-    "locale": "lv"
-}, {
-    "preflang": "my",
-    "locale": "my"
-}, {
-    "preflang": "nl",
-    "locale": "nl"
-}, {
-    "preflang": "no",
-    "locale": "no"
-}, {
-    "preflang": "ph",
-    "locale": "tl-ph"
-}, {
-    "preflang": "pl",
-    "locale": "pl"
-}, {
-    "preflang": "pt",
-    "locale": "pt"
-}, {
-    "preflang": "ro",
-    "locale": "ro"
-}, {
-    "preflang": "rs",
-    "locale": "sr"
-}, {
-    "preflang": "ru",
-    "locale": "ru"
-}, {
-    "preflang": "sk",
-    "locale": "sk"
-}, {
-    "preflang": "sl",
-    "locale": "sl"
-}, {
-    "preflang": "sv",
-    "locale": "sv"
-}, {
-    "preflang": "th",
-    "locale": "th"
-}, {
-    "preflang": "tr",
-    "locale": "tr"
-}, {
-    "preflang": "uk",
-    "locale": "uk"
-}, {
-    "preflang": "vi",
-    "locale": "vi"
-}, {
-    "preflang": "zh",
-    "locale": "zh-cn"
-}, {
-    "preflang": "zs",
-    "locale": "zh-tw"
-}]
+var locales = require('./data/locales');
+var download = require('./data/download');
 
-var errors = require('./errors/errors.js');
-
-for (var i in langs) {
-	var lang = langs[i];
+for (var i in locales) {
+	var locale = locales[i];
 
     try {
-        var variables = require('./variables/vars_' + lang.preflang + '.js');
+        var translations = require('./translations/' + locale.preflang);
     }
     catch (e) {
-        console.log(e)
-        var variables = require('./variables/vars_en.js');
+        console.log(e);
+        var translations = {};
     }
 
-	var out = {
-        "title": variables.title,
-        "country": variables.country,
-        "city": variables.city,
-        "location": variables.loc,
-        "pickUp": variables.pickUp,
-        "dropOff": variables.dropOff,
-        "emptySelect": variables.emptySelect,
-        "pickUpDate": variables.pickUpDate,
-        "dropOffDate": variables.dropOffDate,
-        "time": variables.pickUpTime,
-        "driverAge": variables.driverAge,
-        "search": variables.search,
-        "errorPickUp": errors.pickup[lang.preflang] + ' ' + errors.must[lang.preflang],
-        "errorDropOff": errors.dropoff[lang.preflang] + ' ' + errors.must[lang.preflang],
-        "errorDateDiff": errors.hour[lang.preflang],
-        "errorAge": variables.driverAge + ' ' + errors.must[lang.preflang],
-		"moment": {}
-	}
+    delete translations.errorPickUp;
+    delete translations.errorDropOff;
+    delete translations.errorAge;
+    delete translations.errorDateDiff;
 
-    moment.locale(lang.locale);
-    var localeData = moment.localeData();
+    extend(translations, download[locale.preflang]);
 
-	out.moment.months = localeData._months
-	out.moment.monthsShort = localeData._monthsShort
-	out.moment.weekdays = localeData._weekdays
-	out.moment.weekdaysShort = localeData._weekdaysShort
-	out.moment.weekdaysMin = localeData._weekdaysMin
-	out.moment.longDateFormat = localeData._longDateFormat
+    if (!translations.moment) {
 
-	fs.writeFile("locales/" + lang.preflang + ".json", formatJson.plain(out), function(err) {
+        moment.locale(locale.locale);
+
+        var localeData = moment.localeData();
+
+        translations.moment =  {};
+    	translations.moment.months = localeData._months
+    	translations.moment.monthsShort = localeData._monthsShort
+    	translations.moment.weekdays = localeData._weekdays
+    	translations.moment.weekdaysShort = localeData._weekdaysShort
+    	translations.moment.weekdaysMin = localeData._weekdaysMin
+    	translations.moment.longDateFormat = localeData._longDateFormat
+
+    }
+
+	fs.writeFile("./new/" + locale.preflang + ".json", formatJson.plain(translations), function(err) {
 	    if(err) {
 	        return console.log(err);
 	    }
